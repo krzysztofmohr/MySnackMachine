99bc6ad28009571415904e7bd6009ad25b2a7c57
krzysztof <krzysztof.mohr@live.com>
2018-05-06 23:09:16 +0100

    added slot and snack entities

---
+++ DddInPractice.Logic/Slot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPractice.Logic
+{
+    public class Slot : Entity
+    {
+        public virtual Snack Snack { get; protected set; }
+        public virtual SnackMachine SnackMachine { get; protected set; }
+        public virtual decimal Price { get; protected set; }
+        public virtual int Quantity { get; protected set; }
+        public virtual int Position { get; protected set; }
+
+        protected Slot() {}
+
+        public Slot(SnackMachine snackMachine, Snack snack, decimal price, int quantity, int position) : this()
+        {
+            SnackMachine = snackMachine;
+            Snack = snack;
+            Price = price;
+            Quantity = quantity;
+            Position = position;
+        }
+    }
+}
---
+++ DddInPractice.Logic/Snack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPractice.Logic
+{
+    public class Snack : Entity
+    {
+        public virtual string Name { get; protected set; }
+
+        protected Snack() {}
+
+        public Snack(string name) : this()
+        {
+            this.Name = name;
+        }
+    }
+}
