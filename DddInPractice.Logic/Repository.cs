f3f2eaaf9564e95aa674fd7622977228f3e387d8
krzysztof <krzysztof.mohr@live.com>
2018-06-19 23:47:34 +0100

    added generic repository for aggregate root added db creation scripts

---
+++ DddInPractice.Logic/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+namespace DddInPractice.Logic
+{
+    public abstract class Repository<T> where T: AggregateRoot
+    {
+        public T GetById(long id)
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            {
+                return session.Get<T>(id);
+            }
+        }
+
+        public void Save(T aggregateRoot)
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.SaveOrUpdate(aggregateRoot);
+                transaction.Commit();
+            }
+        }
+    }
+}
