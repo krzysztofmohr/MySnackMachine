fba32de8e781f361932308dcf6313662b319d1ba
krzysztof <krzysztof.mohr@live.com>
2018-08-08 22:55:57 +0100

    added dummy methods for snack machine repository

--- DddInPractice.Logic/SnackMachineRepository.cs
+++ DddInPractice.Logic/SnackMachineRepository.cs
@@ -8,5 +8,7 @@ namespace DddInPractice.Logic
 {
     public class SnackMachineRepository : Repository<SnackMachine>
     {
+        public IReadOnlyList<SnackMachine> GetAllWithSnack(Snack snack) { throw new NotImplementedException(); }
+        public IReadOnlyList<SnackMachine> GetAllWithMoneyInside(Money money) { throw new NotImplementedException(); }
     }
 }
